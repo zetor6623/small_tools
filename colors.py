81790f5d2ac5ae97bbfba2cd5457d529df205e2a
zetor6623 <mentor.91@tlen.pl>
2019-08-03 21:50:49 +0200

    Create colors.py

---
+++ colors.py
@@ -0,0 +1,36 @@
+class Color:
+
+    START = "\033["
+    END = "\033[0m"
+
+    # BACKGROUND
+
+    B_BLACK = ";40m"
+    B_RED = ";41m"
+    B_GREEN = ";42m"
+    B_YELLOW = ";43m"
+    B_BLUE = ";44"
+    B_PURPLE = ";45m"
+    B_CYAN = ";46m"
+    B_WHITE = ";47m"
+    B_NONE = ";48m"
+
+    # COLOR TEXT
+
+    T_BLACK = ";30"
+    T_RED = ";31"
+    T_GREEN = ";32"
+    T_YELLOW = ";33"
+    T_BLUE = ";34"
+    T_PURPLE = ";35"
+    T_CYAN = ";36"
+    T_WHITE = ";37"
+    T_NONE = ";38"
+
+    # STYLE TEXT
+
+    NO_EFFECT = "0"
+    BOLD = "1"
+    UNDERLINE = "2"
+    NEGAT_1 = "3"
+    NEGAT_2 = "5"
